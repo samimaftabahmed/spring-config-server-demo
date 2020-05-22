5efdb04166ea04955766200481cbec7797718c1a
Samim <samimaftabahmed@gmail.com>
2020-05-22 22:57:35 +0530

    Config Server setup

---
+++ src/test/java/com/sam/springconfigserverdemo/SpringConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.springconfigserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
